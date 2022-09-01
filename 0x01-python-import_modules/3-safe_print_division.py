c2944de2bc635a5794f7133d207d9427257a13f6
700P <gogetem@penguin>
2022-09-01 17:39:22 -0500

    m

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
-def safe_print_division(a, b):
-    
+
+def safe_print_division(a, b):    
     try:
         num = a / b
-    except: ZeroDivisionError
+    except: 
         num = None;
-    finally
+        print(ZeroDivisionError)
+    finally:
         print("Inside result: {}".format(num))
-        
+        return num
+
+    if __name__ == "__main__":
+        safe_print_division()
