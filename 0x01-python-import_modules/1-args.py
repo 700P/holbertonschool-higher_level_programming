9273ef79e666147fe0f686170d95793cd1ccb598
700P <gogetem@penguin>
2022-09-01 16:54:29 -0500

    m

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -3,15 +3,15 @@
 if __name__=="__main__":
     import sys
 
-    x = (sys.argv)
+    b = (sys.argv)
     a = len(sys.argv)
-    s = range(1, x)
 
     if a == 0:
         print("{} {}".format(0, "arguments."))
     if a == 1:
         print("{} {}".format(1, "argument:"))
-        print("{}: {}".format(a, x))
+        print("{}: {}".format(1, b[1]))
     elif a == 2:
-        print("{} {}".format(a, "arguments:"))
-        print("{}: {}".format(s, sys.argv[s]))
+        print("{} {}".format(((a) - 1), "arguments:"))
+        for x in range(1, a):
+            print("{}: {}".format(x, sys.argv[x]))
