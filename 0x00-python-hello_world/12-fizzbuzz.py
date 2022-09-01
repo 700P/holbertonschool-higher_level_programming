93294db76694ef7e0a2ec25f576cedbf2031ed94
700P <gogetem@penguin>
2022-09-01 13:20:27 -0500

    m

--- 0x00-python-hello_world/12-fizzbuzz.py
+++ 0x00-python-hello_world/12-fizzbuzz.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-def fizzbuzz(x):
-    print(x)
-
+def fizzbuzz():
     for x in range(100):
+        print(x)
         if x % 3 == 0:
-            print(Fizz)
+            x = Fizz
         if x % 5 == 0: 
-            print(Buzz)
+            x = Buzz
         if x % 5 & 3:
-            print(FizzBuzz)
-            return fizzbuzz()
+            x = FizzBuzz
+        print(x)
---
+++ 0x00-python-hello_world/fizzy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for x in range(100):
+        while x == ((x % 3) == 0):
+                x -> Fizz
+            while x == ((x % 5) == 0)):
+                    x -> Buzz
+                    while x == (x % (3 & 5) == 0 ):
+                            x -> FizzBuzz
+                            print("{}".format(x))
+
