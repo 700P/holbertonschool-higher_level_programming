bd2620316e426badaf77eb25327419bc14899c03
700P <gogetem@penguin>
2022-09-01 23:24:57 -0500

    m

---
+++ 0x02-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    my_list = []
+    
+    for x in range(len(my_list)):
+        idx = list()
+        print("{} {}".format(idx[x], my_list[x]))
