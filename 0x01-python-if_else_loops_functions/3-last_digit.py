c222ad9cf1cb8060a9018763f55fa627c8c20fe5
700P <gogetem@penguin>
2022-09-02 11:12:34 -0500

    m

--- 0x01-python-if_else_loops_functions/3-last_digit.py
+++ 0x01-python-if_else_loops_functions/3-last_digit.py
@@ -4,7 +4,25 @@ number = random.randint(-10000, 10000)
 
 import numpy as np
 amp = np.number
- 
+last = abs(number) % 10
+if last != 0 and number < 0:
+    last *= -1
+    print("last digi of", number, "is", last, end="")
+    if last > 5
+    print("and is greater than 5")
+elif last == 0:
+    print "and is 0"
+    else:
+        print("and is less than 6 and not 0")
+
+
+
+
+
+
+
+
+
 if number > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, amp))
         
@@ -13,4 +31,9 @@ if number == 0:
 
 if number < 6 & number > 0:
         print("Last digit of {} is {} and is less than 6 and not 0".format(number, amp))
+
+
+
+
+int(repr(number[-1]))
 
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    if str == range(97, 123):
-        return(chr(str))
+    if str == range(65, 97):
+
+        (chr(str))
