03fa517e4232754f496e1401c38f06087c6ee7eb
700P <gogetem@penguin>
2022-09-02 01:46:38 -0500

    m

---
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list)
+        return None
+            for x in len(my_list)
+                return(my_list[idx])
+    else:
+        for x in my_list
+        return(append.mylist[element])
