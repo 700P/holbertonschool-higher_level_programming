51161889875fe975398983ceb5d103a0a99d2633
700P <gogetem@penguin>
2022-09-02 22:50:07 -0500

    M

--- 0x00-python-hello_world/8-uppercase.py
+++ 0x00-python-hello_world/8-uppercase.py
@@ -4,4 +4,6 @@
 def uppercase(str):
     for x in str:
         if chr(97) >= x <= chr(123):
-            return(x)
+            x = chr(x) - 32
+        print("{}".format(c), end="")
+        print()
