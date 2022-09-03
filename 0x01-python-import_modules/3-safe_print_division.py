93b1b34b9017cdafeee43fbf716804552d5ca113
700P <gogetem@penguin>
2022-09-02 22:22:18 -0500

    m

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -4,7 +4,7 @@
 def safe_print_division(a, b):
     try:
         num = a / b
-    except: ZeroDivisionError:
+    except: ZeroDivisionError
         num = None
     finally:
         print("Inside result: {}".format(num))
