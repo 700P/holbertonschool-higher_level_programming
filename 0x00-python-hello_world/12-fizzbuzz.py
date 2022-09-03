d294915d7def321f2280b145fc9c054a71a40d43
700P <gogetem@penguin>
2022-09-02 23:08:51 -0500

    m

--- 0x00-python-hello_world/12-fizzbuzz.py
+++ 0x00-python-hello_world/12-fizzbuzz.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
+
 def fizzbuzz():
-    for x in range(100):
-        print(x)
-        if x % 3 == 0:
-            x = Fizz
-        if x % 5 == 0: 
-            x = Buzz
-        if x % 5 & 3:
-            x = FizzBuzz
-        print(x)
+    for x in range(1, 101):
+        if (x % 5 == 0) & (x % 3 == 0):
+            print("FizzBuzz ", end="")
+        elif x % 3 == 0:
+            print("Fizz ", end="")
+        elif x % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(x), end="")
+
+fizzbuzz()
