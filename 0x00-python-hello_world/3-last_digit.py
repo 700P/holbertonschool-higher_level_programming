460e0cbf9f1837146ae51bcb70ff74f38565b935
700P <gogetem@penguin>
2022-09-02 21:52:17 -0500

    M

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,14 +2,19 @@
 import random
 number = random.randint(-10000, 10000)
 
-import numpy as np
-amp = np.number
- 
-if number > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, amp))
-        
-if number == 0:
-    print("Last digit of {} is {} and is 0".format(number, amp))
 
+state = ( "and is greater than 5", "and is 0", "and is less than 6 and not 0")
+if number < 0:
+    amp = (number % -10)
+elif number > 0:
+     amp = (number % 10)
+else:
+   number == 0
+   amp = 0
+
+if number > 5:
+    print("Last digit of {} is {} {} ".format(number, amp, state[0]))
+if number == 0:
+    print("Last digit of {} is {} {} ".format(number, amp, state[1]))
 if number < 6 & number > 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, amp))
+    print("Last digit of {} is {} {} ".format(number, amp, state[2]))
