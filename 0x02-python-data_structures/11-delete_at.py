5c7525f1e3a0d73488fa2c13752eee73d7ce607c
700P <gogetem@penguin>
2022-09-08 22:31:30 -0500

    m

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -2,10 +2,10 @@
 
 
 def delete_at(my_list=[], idx=0):
-     if 0 > idx >= len(my_list):
-         return my_list
-     try:
-         del my_list[idx]
-     except:
-         pass
-     return my_list
+    if 0 > idx >= len(my_list):
+        return my_list
+    try:
+        del my_list[idx]
+    except:
+        pass
+    return my_list
