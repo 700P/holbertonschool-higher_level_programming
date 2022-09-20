f3dc8766b60899549e489bc01483601b64b6980d
700P <gogetem@penguin>
2022-09-20 15:34:15 -0500

    m

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+
+class Base:
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None: 
+            self.id = id
+        else:
+             Base.__nb_objects += 1
+
+             self.id = self.__nb_objects
+
+
+            
+
+# private instance base.__no_objects
+#                   ^   ^ 
+# public instance 'self.id'
+#                  ^   ^
