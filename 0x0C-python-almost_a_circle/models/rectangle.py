2e5411c097a385916f4c5ac09900a3aecdd0a721
700P <gogetem@penguin>
2022-09-22 14:17:25 -0500

    m

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,4 +14,18 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
+@property
+    def width(self)
+        return self.__width
 
+@property
+    def height(self)
+        return self.__height
+
+@property 
+    def x(self)
+        return self.__x
+
+@property 
+    def y(self)
+        return self.__y
