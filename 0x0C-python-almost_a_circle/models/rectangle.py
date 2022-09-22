b61ab4368bb0f652ea9e6da5b5e09c63e3105817
700P <gogetem@penguin>
2022-09-22 13:53:13 -0500

    m

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ from models.base import Base
 class Rectangle(Base):
     """ Rectangle Class """
     def __init__(self, width, height, x=0, y=0, id=None):
-            """ Highway connector __init__ """
+        """ Highway connector __init__ """
         super().__init__(id)
         self.__width = width
         self.__height = height
