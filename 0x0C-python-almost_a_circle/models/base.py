e6b572a9965594be85945477f5bcabb3c3f638a0
700P <gogetem@penguin>
2022-09-22 13:51:42 -0500

    m

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
+""" Base Class for managaging Id of all other classes/ class instances"""
 
 
 class Base:
+    """ Base Clase """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Highway connector __init__ """
         if id is not None: 
             self.id = id
         else:
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-
+""" rectangle metrics """
 
 
 from models.base import Base
 
 class Rectangle(Base):
-
+    """ Rectangle Class """
     def __init__(self, width, height, x=0, y=0, id=None):
-
+            """ Highway connector __init__ """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
 
