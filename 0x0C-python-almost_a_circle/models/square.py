c7e816932ba2e47ecf177d4d74b1fc059b153b3c
700P <gogetem@penguin>
2022-09-23 01:47:53 -0500

    m

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
         self.__width = size
         self.__height = size
 
-    def __str__(self):
+    def __str__(self **kwargs):
         """ change string input """
         return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y, self.size)
 
@@ -25,6 +25,28 @@ class Square(Rectangle):
     def size(self, value):
         """ set the height or width value """
         self.width = value
-        self.height = vlaue
 
 
+    def update(self, *args, **kwargs)
+        """ update the square attribute """
+        if len(args):
+            """args non-kwargs"""
+            for iterarg, arg in enumerate(args):
+                if iterarg == 0:
+                    self.id = arg
+                elif iterarg == 1:
+                    self.size = arg
+                elif iterarg == 2:
+                    self.x = arg
+                elif iterarg == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
