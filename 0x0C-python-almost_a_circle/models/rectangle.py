ee2c39285de04994cae6f46a974d32f88ea0ab66
700P <gogetem@penguin>
2022-09-22 19:35:44 -0500

    m

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,6 @@ class Rectangle(Base):
     def __str__(self):
         """ overide string method """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def def update(self, *args):
+        self.update = args.__str__
