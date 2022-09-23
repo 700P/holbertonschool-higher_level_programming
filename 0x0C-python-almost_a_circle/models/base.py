d7d7a70dc3d7fe731bbcc93729e6dee4910e69fa
700P <gogetem@penguin>
2022-09-23 17:33:32 -0500

    m

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Base Class for managaging Id of all other classes/ class instances """
+import json
 
 
 class Base:
@@ -15,7 +16,15 @@ class Base:
              Base.__nb_objects += 1
 
              self.id = Base.__nb_objects
-
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ json for transposing data """
+        if not list_dictionaries or list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps( list_dictionaries)
+        
 
             
 
