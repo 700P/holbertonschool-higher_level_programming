acd3862211e0abcd022be35aa8e857fa075de229
700P <gogetem@penguin>
2022-09-22 20:53:36 -0500

    m

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,6 +2,8 @@
 """ square class """
 
 
+from models.rectangle import Rectangle
+
 class Square(Rectangle):
     """ inherit the Rectanle attributes """
     def __init__(self, size, x=0, y=0, id=None):
