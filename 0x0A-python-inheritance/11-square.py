2acf1cdaec723380471013dc5103b868d1828385
700P <gogetem@penguin>
2022-09-25 23:58:41 -0500

    m

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#BaseGeometry = __imprt__('9-rectangle').BaseGeometry   creates the inherited instnace of the file and methods 
+
+
+#class Square(Rectangle)
+#   def __init__(self, size)
+
+
+#def  __str__(self):   the __str__ dundar is used mainly for debugging
+   # return ("[Rectangle] {}/{}".format(self.size, self.size)) 
