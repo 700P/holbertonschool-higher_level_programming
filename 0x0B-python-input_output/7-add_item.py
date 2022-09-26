ea41502cb1293488aebdf246aaac2bc0f6ce29b9
700P <gogetem@penguin>
2022-09-26 15:23:58 -0500

    m

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" Write a script that adds all arguments to a Python list, and then save them to a file: """
 
 import sys
 import json
@@ -9,7 +9,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 alist = list(sys.argv[1:])
-
+""" adds all arguments to a Python list """
 try:
     info = load_from_json_file('add_item.json')
 except (TypeError, FileNotFoundError):
--- 0x0B-python-input_output/8-my_class.py
+++ 0x0B-python-input_output/8-my_class.py
@@ -3,12 +3,13 @@
 """
 
 class MyClass:
-    """ My class
-    """
+    """ My class """
 
     def __init__(self, name):
+        """ returns the dictionary description """ 
         self.name = name
         self.number = 0
 
     def __str__(self):
+        """ JSON serialization of an object: """
         return "[MyClass] {} - {:d}".format(self.name, self.number)
