713bde47133316d295b680fc12d0ef4f41ebd560
700P <gogetem@penguin>
2022-09-26 23:52:14 -0500

    M

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+""" Write a class Square that inherits from Rectangle (9-rectangle.py): """
 
+Rectangle = __import__("9-rectangle").Rectangle
+
+class Square(Rectangle):
+    """ class Square that inherits from Rectangle """
+    def __init__(self, size):
+        """ square indentification """
+        self.__size = size
+        self.integer_validator("size", size)
+            
+    def area():
+        """ area a the shape """
+        return (self.__size ** 2)
