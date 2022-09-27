88afca985c29e3bbfdd482117269d57669288bba
700P <gogetem@penguin>
2022-09-27 00:03:25 -0500

    M

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+""" Write a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py). """
 
-#BaseGeometry = __imprt__('9-rectangle').BaseGeometry   creates the inherited instnace of the file and methods 
+Rectangle = __import__("9-rectangle").Rectangle
 
+class Square(Rectangle):
+    """ Square that inherits from Rectangle """
+    def __init__(self, size):
+        """ identify square class """
+        self.integer_validator("size", size)
+        self.__size = size
 
-#class Square(Rectangle)
-#   def __init__(self, size)
+    def __repr__(self):
+        """ prints a Square """
+        return '[Square] {}/{}'.format(self.__size, self.__size)
 
-
-#def  __str__(self):   the __str__ dundar is used mainly for debugging
-   # return ("[Rectangle] {}/{}".format(self.size, self.size)) 
+    def area(self):
+        """ Area of Square """
+        return self.__size ** 2
